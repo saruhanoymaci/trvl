3e1ac624148ff72cdad778a6f17fc4ae50548ae2
Saruhan <saruhan.oymaci@hotmail.com>
2021-08-12 08:21:46 +0300

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import "../../App.css"
 import HeroSection from "../HeroSection"
 import React from 'react'
 import Cards from "../Cards"
-import Footer from "../Footer"
+
 
 function Home() {
     return (
